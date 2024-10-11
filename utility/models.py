85d9633f1e01ec62c9d11b32f0fae900b5406ffa
Behnoush <behnoush.shahraeini@gmail.com>
2024-10-11 22:00:21 +0330

    add is_delete

--- utility/models.py
+++ utility/models.py
@@ -4,6 +4,14 @@ from django.utils import timezone
 class BaseModel(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    
+    is_deleted = models.BooleanField(default=False)
+
     class Meta:
         abstract = True
+        
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        self.save()
+
+    def hard_delete(self, *args, **kwargs):
+        super(BaseModel, self).delete(*args, **kwargs)
--- utility/views.py
+++ utility/views.py
@@ -3,7 +3,8 @@ from rest_framework import generics
 class BaseAPIView(generics.GenericAPIView):
     
     def get_queryset(self):
-        raise NotImplementedError
+        queryset = super().get_queryset()
+        return queryset.filter(is_deleted=False)
 
     def get_serializer_class(self):
         raise NotImplementedError
